939665ec045ca4506d567f44341d3266a1713ef4
irul edgesky <irulannaba@gmail.com>
2024-10-08 19:47:22 +0700

    Placeholder button.

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <button  @click="showModal = true" type="{{}}" onclick="{{$onClick}}" class="bg-{{$color ?? 'blue-500'}} hover:bg-{{$colorHv ?? 'blue-700'}} text-white font-bold py-2 px-4 rounded">
+    <button  @click="showModal = true" type="{{$type ?? 'button'}}" onclick="{{$onClick}}" class="bg-{{$color ?? 'blue-500'}} hover:bg-{{$colorHv ?? 'blue-700'}} text-white font-bold py-2 px-4 rounded">
       {{ $slot }}
     </button>   
 </div>
