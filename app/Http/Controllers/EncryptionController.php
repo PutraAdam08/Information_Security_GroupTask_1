e3a4ec040bbc96ca7052a5c7c5af2370236f2a22
irul edgesky <irulannaba@gmail.com>
2024-10-14 19:53:58 +0700

    Add AES encryption methods to EncryptionController

--- app/Http/Controllers/EncryptionController.php
+++ app/Http/Controllers/EncryptionController.php
@@ -32,4 +32,21 @@ class EncryptionController extends Controller
         $data = substr($data, $iv_length);
         return openssl_decrypt($data, $this->des_method, $key, OPENSSL_RAW_DATA, $iv);
     }
+    public function encrpytAES($data)
+    {
+        $key = $this->keyGeneration(32);
+        $iv_length = openssl_cipher_iv_length($this->aes_method);
+        $iv = openssl_random_pseudo_bytes($iv_length);
+        $encrypted = openssl_encrypt($data, $this->aes_method, $key, OPENSSL_RAW_DATA, $iv);
+        return ['key' => base64_encode($key), 'encrypted' => base64_encode($iv . $encrypted)];
+    }
+    public function decryptAES($encrypted, $key)
+    {
+        $data = base64_decode($encrypted);
+        $key = base64_decode($key);
+        $iv_length = openssl_cipher_iv_length($this->aes_method);
+        $iv = substr($data, 0, $iv_length);
+        $data = substr($data, $iv_length);
+        return openssl_decrypt($data, $this->aes_method, $key, OPENSSL_RAW_DATA, $iv);
+    }
 }
