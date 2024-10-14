4d79bfa68125d4bb9a02016b8492e1d0533c8861
irul edgesky <irulannaba@gmail.com>
2024-10-14 19:59:55 +0700

    Add RC4 encryption method to EncryptionController

--- app/Http/Controllers/EncryptionController.php
+++ app/Http/Controllers/EncryptionController.php
@@ -49,4 +49,40 @@ class EncryptionController extends Controller
         $data = substr($data, $iv_length);
         return openssl_decrypt($data, $this->aes_method, $key, OPENSSL_RAW_DATA, $iv);
     }
+    public function RC4($data, $key)
+    {
+        // Key-scheduling algorithm (KSA)
+        $keyLength = strlen($key);
+        $S = range(0, 255);
+        $j = 0;
+
+        // Initialize permutation in S
+        for ($i = 0; $i < 256; $i++) {
+            $j = ($j + $S[$i] + ord($key[$i % $keyLength])) % 256;
+            // Swap S[i] and S[j]
+            $temp = $S[$i];
+            $S[$i] = $S[$j];
+            $S[$j] = $temp;
+        }
+
+        // Pseudo-random generation algorithm (PRGA)
+        $i = $j = 0;
+        $result = '';
+
+        for ($n = 0; $n < strlen($data); $n++) {
+            $i = ($i + 1) % 256;
+            $j = ($j + $S[$i]) % 256;
+
+            // Swap S[i] and S[j]
+            $temp = $S[$i];
+            $S[$i] = $S[$j];
+            $S[$j] = $temp;
+
+            // Generate the next byte of the key stream and XOR with the data
+            $K = $S[($S[$i] + $S[$j]) % 256];
+            $result .= chr(ord($data[$n]) ^ $K);
+        }
+
+        return $result;
+    }
 }
